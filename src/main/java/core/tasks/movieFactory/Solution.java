a502c164e08bb3c3df2fc9b45bc3a27b2289f81c
Konstantin Besednikov <besednikovkonstantin@gmail.com>
2020-07-01 16:13:16 +0300

    second commit

---
+++ src/main/java/core/tasks/movieFactory/Solution.java
@@ -0,0 +1,24 @@
+package core.tasks.movieFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Movie movie;
+        String key;
+        while (true) {
+            key = reader.readLine();
+            movie = MovieFactory.getMovie(key);
+            if ("soapOpera".equals(key) || "cartoon".equals(key) || "thriller".equals(key)) {
+
+                System.out.println(movie.getClass().getSimpleName());
+            } else break;
+
+
+        }
+
+    }
+}
