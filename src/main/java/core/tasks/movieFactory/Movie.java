54c9c08b275b9c0f3debd6d35894ee7d6f648fe8
Konstantin Besednikov <besednikovkonstantin@gmail.com>
2020-07-01 16:11:47 +0300

    second commit

---
+++ src/main/java/core/tasks/movieFactory/Movie.java
@@ -0,0 +1,11 @@
+package core.tasks.movieFactory;
+
+ abstract class Movie {
+}
+
+ class SoapOpera extends Movie {
+}
+ class Cartoon extends Movie{
+}
+class Thriller extends Movie{
+}
---
+++ src/main/java/core/tasks/movieFactory/MovieFactory.java
@@ -0,0 +1,23 @@
+package core.tasks.movieFactory;
+
+ class MovieFactory {
+
+    static Movie getMovie(String key) {
+        Movie movie = null;
+
+        //создание объекта SoapOpera (мыльная опера) для ключа "soapOpera"
+        if ("soapOpera".equals(key)) {
+            movie = new SoapOpera();
+        }
+        else if("cartoon".equals(key)){
+            movie = new Cartoon();
+        }
+        else if("thriller".equals(key)){
+            movie = new Thriller();
+        }
+
+        //напишите тут ваш код, пункты 5,6
+
+        return movie;
+    }
+}
